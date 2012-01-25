d92e662b0d7a7aabcda38ea4141b2b76a09c967b
Shajed Evan <evanshajed@gmail.com>
2012-01-26 00:55:35 +0600

    Featured Segment
    Featured Segment done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-/* Author:
+/* Author: Shajed Evan
 
 */
+
+$(document).ready(function() {
+    setHeight('.featuredService');
+});
+
+var maxHeight = 0;
+function setHeight(column) {
+    //Get all the element with class = col
+    column = $(column);
+    //Loop all the column
+    column.each(function() {       
+        //Store the highest value
+        if($(this).height() > maxHeight) {
+            maxHeight = $(this).height();;
+        }
+    });
+    //Set the height
+    column.height(maxHeight);
+}
 
 
 
