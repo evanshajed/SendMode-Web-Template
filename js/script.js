7f492dcc446a42c71ba5f5c8a3ce4ea4585c5a29
Shajed Evan <evanshajed@gmail.com>
2012-02-02 03:31:29 +0600

    Final Touch
    Navigation Changes, Color Changes, Slider Color Changes. Minor IE fixes
    has been made.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@
 */
 
 $(document).ready(function() {
+
+	$("#nav li").hover(function(){
+		$(this).find('ul:first').css({visibility: "visible",display: "none"}).show(150);
+		},function(){
+		$(this).find('ul:first').css({visibility: "hidden"});
+		});
+
     setHeight('.featuredService');
     	$('.slidedeck').slidedeck({
                 autoPlay: true,
